e549ab0a0455cfd9d1f1c0ae07ff5597dfb95015
nathanduchesne <61148298+nathanduchesne@users.noreply.github.com>
2022-03-07 13:57:11 +0100

    Source code and tests (100% coverage) for Song and Playlist DS
    If applied, this commit will add the possibility of creating playlists, which contain songs. Songs are equal if their artist name and title are identical, because we can safely assume an artist doesn't have two songs with the same name. Playlists can be added to one another.

---
+++ app/src/main/java/com/github/fribourgsdp/radio/Genre.kt
@@ -0,0 +1,9 @@
+enum class Genre (val genreName: String){
+    POP("POP"),
+    HIPHOP("HIP-HOP"),
+    ROCK("ROCK1"),
+    LATIN("LATIN"),
+    KPOP("K-POP"),
+    COUNTRY("COUNTRY"),
+    NONE("No Specific Genre")
+}
