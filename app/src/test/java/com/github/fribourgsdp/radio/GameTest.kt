48a1e8ac9ff9f49e38b9676dd17d07540ec58413
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-03-10 17:52:49 +0100

    Implement first test for Game builder

---
+++ app/src/test/java/com/github/fribourgsdp/radio/GameTest.kt
@@ -0,0 +1,45 @@
+package com.github.fribourgsdp.radio
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GameTest {
+    private val song1 = Song("colorado", "Milky Chance")
+    private val song2 = Song("Back in black", "ACDC")
+    private val song3 = Song("i got a feeling", "black eyed pees")
+    private val song4 = Song("  party rock anthem", "lmfao")
+    private val playlistTest = Playlist("Test",
+        mutableSetOf(song1, song2, song3, song4),
+        Genre.NONE
+    )
+
+    @Test
+    fun builderWorksCorrectlyWithCorrectArgs() {
+        val builder = Game.Builder()
+        val name = "Game Test"
+        val host = User()
+        val other = User()
+        val nbRounds = 3
+        val withHint = true
+        val isPrivate = false
+
+        builder.setHost(host)
+            .setName(name)
+            .setPlaylist(playlistTest)
+            .setWithHint(withHint)
+            .setNbRounds(nbRounds)
+            .setPrivacy(isPrivate)
+            .addUser(other)
+
+        val game = builder.build()
+
+        assertEquals(name, game.name)
+        assertEquals(host, game.host)
+        assertEquals(nbRounds, game.nbRounds)
+        assertEquals(withHint, game.withHint)
+        assertEquals(isPrivate, game.isPrivate)
+        assertEquals(false, game.isDone)
+        assertEquals(playlistTest, game.playlist)
+    }
+}
