ba94009df8dee8bd6be0c7fb347871ba9742b6f0
Shanzhaiii <alexandre.riou@epfl.ch>
2022-03-10 18:16:39 +0100

    add user data structure

---
+++ app/src/main/java/com/github/fribourgsdp/radio/User.kt
@@ -0,0 +1,44 @@
+package com.github.fribourgsdp.radio
+
+import android.graphics.Color
+import java.lang.IllegalArgumentException
+import kotlin.random.Random
+
+class User (nameInput: String) {
+    var name: String = nameInput
+        set(value) {
+            if (value.isEmpty() || value.isBlank()) {
+                throw  IllegalArgumentException("name cannot be empty or blank string")
+            }
+            field = value
+        }
+    private val playlists = mutableSetOf<Playlist>()
+    val color : Int = Color.rgb(
+        Random.nextInt(200, 255),
+        Random.nextInt(200, 255),
+        Random.nextInt(200, 255))
+    var linkedSpotify: Boolean = false
+
+
+    fun initial(): Char = name.elementAt(0)
+
+    fun addPlaylist(playlist: Playlist){
+        playlists.add(playlist)
+    }
+
+    fun addPlaylists(addedPlaylists: Set<Playlist>){
+        playlists.addAll(addedPlaylists)
+    }
+
+    fun removePlaylist(playlist: Playlist){
+        playlists.remove(playlist)
+    }
+
+    fun removePlaylists(removedPlaylist: Set<Playlist>){
+        playlists.removeAll(removedPlaylist)
+    }
+
+    fun getPlaylists(): Set<Playlist> {
+        return playlists.toSet()
+   }
+}
