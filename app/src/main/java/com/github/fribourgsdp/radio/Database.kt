fe26914acda00d54bceaa579fbdc00647c4df6b5
bart <lucien.bart@epfl.ch>
2022-03-15 20:48:25 +0100

    Add documentation to Database class

--- app/src/main/java/com/github/fribourgsdp/radio/Database.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/Database.kt
@@ -10,20 +10,31 @@ import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+
 import java.util.concurrent.CompletableFuture
 
+/**
+ *
+ *This class represents a database. It communicates with the database(Firestore)
+ * and translates the result in classes
+ *
+ * @constructor Creates a database linked to Firestore
+ */
 class Database {
     private val db = Firebase.firestore
 
-
+    /**
+     * Gets the [User], wrapped in a [Task], linked to the [userId] given.
+     * @return [User] wrapped in a task, if the [userId] doesn't exists, it returns a TODO
+     *
+     */
      fun getUser(userId : String): Task<User> {
          return  db.collection("user").document(userId).get().continueWith(Continuation { l-> User(l.result["first"].toString()) })
      }
 
+    /**
+     * Sets the [User] information in the database and link it the to [userId] given
+     */
     fun setUser(userId : String, user : User){
         val user = hashMapOf(
             "first" to user.name
