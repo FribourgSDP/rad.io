4767306b2eadc392388917f866926ce154d06d8d
bart <lucien.bart@epfl.ch>
2022-03-16 18:41:03 +0100

    Slight fixes in DataBaseTest

--- app/src/androidTest/java/com/github/fribourgsdp/radio/DatabaseTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/DatabaseTest.kt
@@ -8,27 +8,21 @@ import java.util.concurrent.TimeUnit
 
 class DatabaseTest {
     private val userAuthUID = "testUser"
-    /*@BeforeAll
-    fun beforeAll() {
-        val db = Database()
-    }*/
+   
     @Test
     fun registeringUsingAndFetchingItWorks(){
         val db = Database()
-        //FirebaseAuth.getInstance().currentUser?.uid
-        // Create a new user with a first and last name
-        var name = "TestingThings"
+        val name = "TestingThings"
         val userTest = User(name)
         db.setUser(userAuthUID,userTest)
-
-        var user = Tasks.withTimeout(db.getUser(userAuthUID),10,TimeUnit.SECONDS)
+        val user = Tasks.withTimeout(db.getUser(userAuthUID),10,TimeUnit.SECONDS)
         assert(Tasks.await(user).name == name)
     }
+
     @Test
     fun fetchingUnregisteredUserReturnsNull(){
-
         val db = Database()
-        var user = Tasks.withTimeout(db.getUser("rubbish"),10,TimeUnit.SECONDS)
+        val user = Tasks.withTimeout(db.getUser("rubbish"),10,TimeUnit.SECONDS)
         Log.d(ContentValues.TAG, "DocumentSnapshot added with ID put: " + Tasks.await(user))
         assert(Tasks.await(user) == null)
     }
