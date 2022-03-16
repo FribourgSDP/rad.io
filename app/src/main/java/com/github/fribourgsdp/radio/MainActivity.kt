75a25f5180bdc79f050b32183f67d238b3a21f8e
bart <lucien.bart@epfl.ch>
2022-03-16 20:47:05 +0100

    Reduce size of the main

--- app/src/main/java/com/github/fribourgsdp/radio/MainActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/MainActivity.kt
@@ -19,28 +19,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
         val fireBaseButton = findViewById<Button>(R.id.FireBaseButton)
         fireBaseButton.setOnClickListener{
             startActivity(Intent(this,FireBaseTestActivity::class.java))
         }
-
         val playButton = findViewById<Button>(R.id.playButton)
         playButton.setOnClickListener {
             startActivity(Intent(this, GameSettingsActivity::class.java))
         }
-
         val settingsButton = findViewById<Button>(R.id.settingsButton)
         settingsButton.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
-        
         val button : Button = findViewById(R.id.button)
         button.setOnClickListener {
             startActivity(Intent(this, DisplayLyricsActivity::class.java))
         }
-
         val profileButton: ImageButton = findViewById(R.id.profileButton)
         profileButton.setOnClickListener {
             val intent : Intent = Intent(this, UserProfileActivity::class.java).apply {
@@ -48,6 +42,5 @@ class MainActivity : AppCompatActivity() {
             }
             startActivity(intent)
         }
-
     }
 }
