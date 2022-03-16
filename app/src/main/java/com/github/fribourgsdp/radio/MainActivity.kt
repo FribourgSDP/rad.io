49196758e6e44bba18ec0f5eb68f816b5fe5161e
bart <lucien.bart@epfl.ch>
2022-03-16 20:57:21 +0100

    Delete useless line

--- app/src/main/java/com/github/fribourgsdp/radio/MainActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/MainActivity.kt
@@ -14,8 +14,6 @@ const val USERNAME = "com.github.fribourgsdp.radio.USERNAME"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
