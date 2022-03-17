d2548b2f39488e8bf03c50600fbb789899207f3d
nathanduchesne <61148298+nathanduchesne@users.noreply.github.com>
2022-03-17 18:34:47 +0100

    Deleted files originally used for testing my implementation in real time

--- app/src/androidTest/java/com/github/fribourgsdp/radio/UserProfileActivityTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/UserProfileActivityTest.kt
@@ -3,6 +3,8 @@ package com.github.fribourgsdp.radio
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.init
+import androidx.test.espresso.intent.Intents.release
 import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -16,6 +18,7 @@ class UserProfileActivityTest : TestCase() {
     val testRule = ActivityScenarioRule(
         UserProfileActivity::class.java
     )
+
 
     @Test
     fun testBuildReqest() {
