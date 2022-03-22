c076ac6a3b8eff33f4aed0c56724de819e1f6165
Simon A. Marti <simon.marti@epfl.ch>
2022-03-22 11:47:43 +0100

    Add compare string methods & tests

--- app/src/androidTest/java/com/github/fribourgsdp/radio/StringComparisonsTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/StringComparisonsTest.kt
@@ -1,4 +1,56 @@
 package com.github.fribourgsdp.radio
 
-class StringComparisons {
+import org.junit.Test
+
+class StringComparisonsTest {
+    @Test
+    fun equalStringsTest(){
+        assert(100 == StringComparisons.compare("rouge", "rouge", 100))
+    }
+    @Test
+    fun spacefulStringsTest(){
+        assert(100 == StringComparisons.compare("  wish you    were  here    ", "wish you were here", 100))
+    }
+    @Test
+    fun differentCasesStringsTest(){
+        assert(100 == StringComparisons.compare("  Wish you    WERE  herE    ", "wish you were here", 100))
+    }
+    @Test
+    fun veryDecoratedStringsTest(){
+        assert(100 == StringComparisons.compare("  Wish, you  ~  WERE-herE    ", "Wish you were here...", 100))
+    }
+    @Test
+    fun accentedStrings(){
+        assert(100 == StringComparisons.compare("àäầéèêẽ-íïőucç", "aaaeeee_iiouçĉ", 100))
+    }
+    @Test
+    fun oneDifferenceStrings1(){
+        assert(100 == StringComparisons.compare("rovge", "rouge", 100, 1))
+        assert(100 == StringComparisons.compare("rougf ", "rouge", 100, 1))
+        assert(100 == StringComparisons.compare("rovge", "rouge", 100, 1))
+        assert(100 == StringComparisons.compare("  Wish, yoO  ~  WERE-herE    ", "Wish you were here...", 100, 1))
+        assert(100 == StringComparisons.compare("àûầéèêẽ-íïöucç", "aaaeeee_iiouçĉ", 100, 1))
+        }
+    @Test
+    fun oneDifferenceStrings2(){
+        assert(100 == StringComparisons.compare("roge", "rouge", 100, 1))
+        assert(100 == StringComparisons.compare("rouuge", "rouge", 100, 1))
+        assert(100 == StringComparisons.compare("master of pupppets", "master of puppets", 100, 1))
+        assert(100 == StringComparisons.compare("master of pupets", "master of puppets", 100, 1))
+        assert(100 == StringComparisons.compare("master of puppet", "master of puppets", 100, 1))
+    }
+    @Test
+    fun twoDifferencesStrings2(){
+        assert(100 == StringComparisons.compare("rge", "rouge", 100, 2))
+        assert(100 == StringComparisons.compare("rouuuge", "rouge", 100, 2))
+        assert(100 == StringComparisons.compare("mastwr og puppets", "master of puppets", 100, 2))
+        assert(100 == StringComparisons.compare("master of pupet", "master of puppets", 100, 2))
+        assert(100 == StringComparisons.compare("master of puppppets", "master of puppets", 100, 2))
+    }
+    @Test
+    fun differentStrings(){
+        assert(0 == StringComparisons.compare("rge", "rouge", 100, 1))
+        assert(0 == StringComparisons.compare("rougf", "rouge", 100, 0))
+    }
+
 }
--- app/src/main/java/com/github/fribourgsdp/radio/StringComparisons.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/StringComparisons.kt
@@ -1,4 +1,54 @@
 package com.github.fribourgsdp.radio
 
+import java.text.Normalizer
+
 class StringComparisons {
+    companion object{
+        private val chars = "abcdefghijklmnopqrstuvwxyz1234567890"
+        fun compare(actual : String, expected : String, maxPts : Int = 100, tolerance : Int = 0) : Int {
+            val s1 = actual.replace(Regex("[!-/]|[:-@]|[\\[-`]|[{-~]| +"), "").lowercase().unaccent()
+            val s2 = expected.replace(Regex("[!-/]|[:-@]|[\\[-`]|[{-~]| +"), "").lowercase().unaccent()
+            if(equal(s1, s2, tolerance)){
+                return maxPts
+            }
+            return 0
+        }
+        private fun CharSequence.unaccent() : String{
+            val temp = Normalizer.normalize(this, Normalizer.Form.NFD)
+            return "\\p{InCombiningDiacriticalMarks}+".toRegex().replace(temp, "")
+        }
+        private fun equal(s1 : String, s2 : String, tolerance: Int) : Boolean {
+            if (tolerance >= 0){
+                if (s1 == s2){
+                    return true
+                }
+            }
+            if (tolerance >= 1){
+                if (s1.length == s2.length){
+                    for (i in s1.indices){
+                        for (c in chars){
+                            if (equal(s1.substring(0, i) + c + s1.substring(i+1, s1.length), s2, tolerance-1)){
+                                return true
+                            }
+                        }
+                    }
+                }else if (s1.length < s2.length){
+                    for (i in s2.indices){
+                        if (equal(s1, s2.substring(0, i) + s2.substring(i+1, s2.length), tolerance-1)){
+                            return true
+                        }
+                    }
+                }else if (s1.length > s2.length){
+                    for (i in s2.indices){
+                        for (c in chars) {
+                            if (equal(s1, s2.substring(0, i) + c + s2.substring(i, s2.length), tolerance-1)) {
+                                return true
+                            }
+                        }
+                    }
+                }
+            }
+            return false
+        }
+    }
 }
