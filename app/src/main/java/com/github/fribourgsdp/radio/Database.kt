2d57614ea2fe5349ccc7bd5ec7a81b2470b702c1
bart <lucien.bart@epfl.ch>
2022-03-22 10:11:37 +0100

    Add get/set for playlist and song

--- app/src/main/java/com/github/fribourgsdp/radio/Database.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/Database.kt
@@ -31,15 +31,14 @@ class Database {
      *
      */
      fun getUser(userId : String): Task<User> {
-
-         return  db.collection("user").document(userId).get().continueWith { l ->
-             val found = l.result["first"].toString()
-             if (found == "null"){
-                 null
-             }else{
-                 User(found)
-             }
-         }
+        return  db.collection("user").document(userId).get().continueWith { l ->
+            val result = l.result
+            if(result.exists()){
+                User(result["first"].toString())
+            }else{
+                null
+            }
+        }
     }
 
     /**
@@ -50,6 +49,68 @@ class Database {
             "first" to user.name
         )
         db.collection("user").document(userId).set(userHash)
+    }
+
+
+    fun getSong(songName : String): Task<Song>{
+        return  db.collection("songs").document(songName).get().continueWith { l ->
+            val result = l.result
+            if(result.exists()){
+                val songName = result["songName"].toString()
+                val artistName = result["artistName"].toString()
+                Song(songName,artistName,"")
+            }else{
+                null
+            }
+        }
+    }
+
+    fun registerSong(song : Song){
+        val songHash = hashMapOf(
+            "songName" to song.name,
+            "artistName" to song.artist
+            //todo: add lyrics when it won't be a future anymore
+        )
+        db.collection("songs").document(song.name).set(songHash)
+
+    }
+
+    fun getPlaylist(playlistName : String): Task<Playlist>{
+
+        return  db.collection("playlists").document(playlistName).get().continueWith { l ->
+            val result = l.result
+            if(result.exists()){
+
+                val playlistTitle = result["playlistName"].toString()
+                val genre =result["genre"].toString()
+                val songs = result["songs"].toString()
+
+                //parse the list result to create a set
+                val songSet : MutableSet<Song> = mutableSetOf()
+                for(song in songs.substring(1,songs.length-1).split(",")){
+                    songSet.add((Song(song,"","")))
+                }
+
+                Playlist(playlistTitle, songSet, Genre.valueOf(genre))
+
+            }else{
+                null
+            }
+        }
+    }
+
+    fun registerPlaylist( playlist : Playlist){
+        //We will only store the songName, because you don't want to fetch all the lyrics of all songs when you retrieve the playlist
+        val titleList : MutableList<String> = mutableListOf()
+        for( song in playlist.getSongs()){
+            titleList.add(song.name)
+        }
+        val playlistHash = hashMapOf(
+            "playlistName" to playlist.name,
+            "genre" to playlist.genre,
+            "songs" to titleList
+        )
+        db.collection("playlists").document(playlist.name).set(playlistHash)
     }
 
 }
