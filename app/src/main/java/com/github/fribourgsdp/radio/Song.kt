f9f52ce947278b74913ceb68d2a726894caf1d2f
Shanzhaiii <alexandre.riou@epfl.ch>
2022-03-22 14:32:31 +0100

    remove useless secondary lyrics parameter

--- app/src/main/java/com/github/fribourgsdp/radio/Song.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/Song.kt
@@ -8,7 +8,6 @@ import java.util.concurrent.CompletableFuture
 class Song (private val rawName: String, private val rawArtist: String, var lyrics: String) {
     val name: String = reformatName(rawName)
     val artist: String = reformatName(rawArtist)
-    private var userProvidedLyrics : String = ""
 
     constructor(name: String, artist: String): this(name, artist,"")
     constructor(name: String, artist: String, lyrics: CompletableFuture<String>): this(name, artist, lyrics.get())
