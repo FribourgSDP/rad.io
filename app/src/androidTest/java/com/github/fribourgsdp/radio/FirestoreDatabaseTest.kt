c2eca961ff9c44bee826aefe1b8dc97c136cbadb
bart <lucien.bart@epfl.ch>
2022-03-23 20:18:29 +0100

    Refactor old Database class test into this class

---
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/FirestoreDatabaseTest.kt
@@ -0,0 +1,30 @@
+package com.github.fribourgsdp.radio
+
+import android.content.ContentValues
+import android.util.Log
+import com.google.android.gms.tasks.Tasks
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class FirestoreDatabaseTest {
+    private val userAuthUID = "testUser"
+   
+    @Test
+    fun registeringUsingAndFetchingItWorks(){
+        val db = FirestoreDatabase()
+        val name = "TestingThings"
+        val userTest = User(name)
+        db.setUser(userAuthUID,userTest)
+        val user = Tasks.withTimeout(db.getUser(userAuthUID),10,TimeUnit.SECONDS)
+        assert(Tasks.await(user).name == name)
+    }
+
+    @Test
+    fun fetchingUnregisteredUserReturnsNull(){
+        val db = FirestoreDatabase()
+        val user = Tasks.withTimeout(db.getUser("rubbish"),10,TimeUnit.SECONDS)
+        Log.d(ContentValues.TAG, "DocumentSnapshot added with ID put: " + Tasks.await(user))
+        assert(Tasks.await(user) == null)
+    }
+
+}
