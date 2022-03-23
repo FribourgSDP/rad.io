7c3e991a23c87f8e64d05c26b964f83bca4efad2
bart <lucien.bart@epfl.ch>
2022-03-23 20:19:15 +0100

    Change return type

--- app/src/main/java/com/github/fribourgsdp/radio/Database.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/Database.kt
@@ -8,32 +8,32 @@ interface Database {
      * Gets the [User], wrapped in a [Task], linked to the [userId] given, [userId] should be the authentication token
      * @return [User] wrapped in a task, if the [userId] doesn't exists, it returns a null [User]
      */
-   fun getUser(userId : String): Task<User?>
+   fun getUser(userId : String): Task<User>
 
    /**
     * Sets the [User] information in the database and link it the to [userId] given, [userId] should be the authentication token
     */
-   fun setUser(userId : String, user : User)
+   fun setUser(userId : String, user : User): Task<Void>
 
     /**
      * Gets the [Song], wrapped in a [Task], given its [songName]
      * @return [Song], wrapped in a [Task], the [Song] is null if it doesn't exist
      */
-   fun getSong(songName : String): Task<Song?>
+   fun getSong(songName : String): Task<Song>
 
     /**
      * Register the [Song] in the database
      */
-   fun registerSong(song : Song)
+   fun registerSong(song : Song): Task<Void>
 
     /**
      * Get the [Playlist], wrapped in a [Task], given its [playlistName]
      * @return [Playlist], wrapped in a [Task], the [Playlist] is null if it doesn't exist
      */
-   fun getPlaylist(playlistName : String): Task<Playlist?>
+   fun getPlaylist(playlistName : String): Task<Playlist>
 
     /**
      * Register the [Playlist] in the database
      */
-   fun registerPlaylist( playlist : Playlist)
+   fun registerPlaylist( playlist : Playlist): Task<Void>
 }
