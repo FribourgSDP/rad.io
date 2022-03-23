66c86065d56e60ba67d2f240a3406a07612b4b42
bart <lucien.bart@epfl.ch>
2022-03-23 20:25:05 +0100

    Adapt to new return type and modify registerPlaylist so that it run the same as Firestore

--- app/src/main/java/com/github/fribourgsdp/radio/LocalDatabase.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/LocalDatabase.kt
@@ -2,6 +2,7 @@ package com.github.fribourgsdp.radio
 
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
+import java.lang.IllegalArgumentException
 
 class LocalDatabase : Database {
     val userMap: MutableMap<String, User> = mutableMapOf()
@@ -9,27 +10,35 @@ class LocalDatabase : Database {
     val playlistMap: MutableMap<String,Playlist> = mutableMapOf()
 
 
-    override fun getUser(userId: String): Task<User?> {
+    override fun getUser(userId: String): Task<User> {
         return Tasks.forResult(userMap[userId])
     }
 
-    override fun setUser(userId: String, user: User) {
-        userMap[userId] = user
+    override fun setUser(userId: String, user: User): Task<Void> {
+        userMap[userId] = User(user.name)
+        return Tasks.forResult(null)
     }
 
-    override fun getSong(songName: String): Task<Song?> {
+    override fun getSong(songName: String): Task<Song> {
         return Tasks.forResult(songMap[songName])
     }
 
-    override fun registerSong(song: Song) {
+    override fun registerSong(song: Song): Task<Void> {
         songMap[song.name] = song
+        return Tasks.forResult(null)
     }
 
-    override fun getPlaylist(playlistName: String): Task<Playlist?> {
+    override fun getPlaylist(playlistName: String): Task<Playlist> {
         return Tasks.forResult(playlistMap[playlistName])
     }
 
-    override fun registerPlaylist(playlist: Playlist) {
-        playlistMap[playlist.name] = playlist
+    override fun registerPlaylist(playlist: Playlist): Task<Void> {
+        val titleList : MutableList<Song> = mutableListOf()
+        for( song in playlist.getSongs()){
+            titleList.add(Song(song.name,song.artist,""))
+        }
+        playlistMap[playlist.name] = Playlist(playlist.name,titleList.toSet(),playlist.genre)
+
+        return Tasks.forResult(null)
     }
 }
