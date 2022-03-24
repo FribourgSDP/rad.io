cff66563d9acd7958930caeca1d9b275ed24cad1
Shanzhaiii <alexandre.riou@epfl.ch>
2022-03-24 08:49:55 +0100

    add SongAdapter

--- app/src/main/java/com/github/fribourgsdp/radio/PlaylistDisplayActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/PlaylistDisplayActivity.kt
@@ -3,6 +3,8 @@ package com.github.fribourgsdp.radio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
@@ -15,5 +17,11 @@ class PlaylistDisplayActivity : AppCompatActivity() {
 
         val playlistName : TextView = findViewById(R.id.PlaylistName)
         playlistName.text = playlist.name
+
+        val songs = playlist.getSongs().toList()
+        val songDisplay : RecyclerView = findViewById(R.id.SongRecyclerView)
+        songDisplay.adapter = SongAdapter(songs)
+        songDisplay.layoutManager = (LinearLayoutManager(this))
+        songDisplay.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/github/fribourgsdp/radio/SongAdapter.kt
@@ -0,0 +1,31 @@
+package com.github.fribourgsdp.radio
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class SongAdapter(private val songList: List<Song>) :
+    RecyclerView.Adapter<SongAdapter.SongViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.song_item,
+            parent, false)
+        return SongViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = songList[position]
+
+        holder.titleView.text = song.name
+        holder.subtitleView.text = song.artist
+    }
+
+    override fun getItemCount(): Int = songList.size
+
+    inner class SongViewHolder(itemView: View) :    RecyclerView.ViewHolder(itemView){
+        val titleView: TextView = itemView.findViewById(R.id.song_title_view)
+        val subtitleView: TextView = itemView.findViewById(R.id.song_subtitle_view)
+    }
+}
