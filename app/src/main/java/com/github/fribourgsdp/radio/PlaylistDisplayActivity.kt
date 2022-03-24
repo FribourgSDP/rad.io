3ba5993591f2ce16b245feb06b08d7aeb04acbbb
Shanzhaiii <alexandre.riou@epfl.ch>
2022-03-24 03:23:13 +0100

    create PlaylistDisplayActivity and its ressources

---
+++ app/src/main/java/com/github/fribourgsdp/radio/PlaylistDisplayActivity.kt
@@ -0,0 +1,11 @@
+package com.github.fribourgsdp.radio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaylistDisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_playlist_display)
+    }
+}
