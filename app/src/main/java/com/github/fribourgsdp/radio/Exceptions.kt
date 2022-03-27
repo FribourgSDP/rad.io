62e34119ea72a4efbad648f531d1fb987c2af348
bart <lucien.bart@epfl.ch>
2022-03-27 18:29:35 +0200

    Create some Exceptions

---
+++ app/src/main/java/com/github/fribourgsdp/radio/Exceptions.kt
@@ -0,0 +1,15 @@
+package com.github.fribourgsdp.radio
+
+class UserNotFoundException : Exception("User was not found")
+
+class SongNotFoundException : Exception("Song was not found")
+
+class PlaylistNotFoundException : Exception("Playlist was not found")
+
+class LobbyNotFoundException  : Exception("Lobby was not found")
+
+class LobbyNotAvailableException  : Exception("Lobby was not availabe")
+
+class LyricsNotFoundException : Exception("No lyrics were found")
+
+
