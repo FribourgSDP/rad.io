48a219cab1a983cd2c83f3dab51fc96164c2a0d4
nathanduchesne <61148298+nathanduchesne@users.noreply.github.com>
2022-03-29 16:10:54 +0200

    Fixed typo

--- app/src/androidTest/java/com/github/fribourgsdp/radio/UserProfileActivityTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/UserProfileActivityTest.kt
@@ -25,7 +25,7 @@ import java.util.concurrent.TimeUnit
 @RunWith(AndroidJUnit4::class)
 class UserProfileActivityTest : TestCase() {
     @get:Rule
-    var userProfileActivityRule = ActivityScenarioRule(UserProfileActivityt::class.java)
+    var userProfileActivityRule = ActivityScenarioRule(UserProfileActivity::class.java)
 
     @Test
     fun logoutButtonTest() {
