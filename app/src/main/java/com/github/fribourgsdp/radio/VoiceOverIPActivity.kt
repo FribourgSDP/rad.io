90de0d3295c8bd59872d7d36a69f2bbbe02b3b7a
mmauron <maxence.mauron@epfl.ch>
2022-03-30 21:51:45 +0200

    Create personal token for agora

--- app/src/main/java/com/github/fribourgsdp/radio/VoiceOverIPActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/VoiceOverIPActivity.kt
@@ -10,17 +10,23 @@ import android.content.Intent
 import java.lang.Exception
 import io.agora.rtc.RtcEngine
 import io.agora.rtc.IRtcEngineEventHandler
+import RtcTokenBuilder.RtcTokenBuilder
+
 
 class VoiceOverIPActivity : AppCompatActivity() {
 
     private val APP_ID = "971046257e964b73bafc7f9458fa9996"
-    private val CHANNEL = "testNathanMaxence"
+    private val CHANNEL = "test44"
     private val CERTIFICATE = "5822360c68714dcca1382167d4070673"
-    private val TOKEN = "006971046257e964b73bafc7f9458fa9996IAAbJr0QnsdkBXv2l5DoNI6N/JcK5z0gdkxdY24umGNotRwU6J0AAAAAEAAg4mLWLjNDYgEAAQAuM0Ni"
+    private val TOKEN = "006971046257e964b73bafc7f9458fa9996IABHFc2VemmVSekHkloRBGwdRXrfr49nBFNBC2+4szfUboESWpqbjtJtIgB5s8RbnuNFYgQAAQBNvERiAgBNvERiAwBNvERiBABNvERi"
+    private val EXPIRATION_TIME = 10800
+
     private var mRtcEngine: RtcEngine ?= null
     private val mRtcEventHandler = object : IRtcEngineEventHandler() {
     }
 
+    private val uid = 1
+    private lateinit var token : String
     private val PERMISSION_REQ_ID_RECORD_AUDIO = 22
     private val PERMISSION_REQ_ID_CAMERA = PERMISSION_REQ_ID_RECORD_AUDIO + 1
     /*
@@ -45,7 +51,9 @@ class VoiceOverIPActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_voice_over_ipactivity)
+        token = getToken(uid)
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO, PERMISSION_REQ_ID_RECORD_AUDIO)) {
+
             initializeAndJoinChannel();
         }
     }
@@ -55,7 +63,9 @@ class VoiceOverIPActivity : AppCompatActivity() {
             mRtcEngine = RtcEngine.create(baseContext, APP_ID, mRtcEventHandler)
         } catch (e: Exception) {
         }
-        mRtcEngine!!.joinChannel(TOKEN, CHANNEL, "", 0)
+
+        mRtcEngine!!.joinChannel(token, CHANNEL, "", uid)
+
     }
 
     
@@ -70,5 +80,17 @@ class VoiceOverIPActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+
+    private fun getToken(uid:Int) : String{
+        val token = RtcTokenBuilder()
+        val timestamp = (System.currentTimeMillis() / 1000 + EXPIRATION_TIME).toInt()
+
+        val result = token.buildTokenWithUid(
+            APP_ID, CERTIFICATE,
+            CHANNEL, uid, RtcTokenBuilder.Role.Role_Publisher, timestamp
+        )
+        return result
     }
 }
