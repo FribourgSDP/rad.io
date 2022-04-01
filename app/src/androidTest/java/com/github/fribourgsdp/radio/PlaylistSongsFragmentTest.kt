d88ca476ab3ffaf6f7f67e665c8c62ad8b15dce5
Alexandre Riou <alexandre.riou@epfl.ch>
2022-04-01 07:21:55 +0200

    add test for recyclerview presence in PlaylistSongsFragmentTest

--- app/src/androidTest/java/com/github/fribourgsdp/radio/PlaylistSongsFragmentTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/PlaylistSongsFragmentTest.kt
@@ -3,8 +3,12 @@ package com.github.fribourgsdp.radio
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -29,5 +33,15 @@ class PlaylistSongsFragmentTest {
         val scenario = launchFragmentInContainer<PlaylistSongsFragment>(bundle)
         Espresso.onView(withId(R.id.PlaylistName))
             .check(ViewAssertions.matches(ViewMatchers.withText(playlistName)))
+    }
+
+    @Test
+    fun playListFragmentHasRecyclerView() {
+        val bundle = Bundle()
+        val playlistName = "test"
+        val playlist : Playlist = Playlist(playlistName, Genre.NONE)
+        bundle.putString(PLAYLIST_DATA, Json.encodeToString(playlist))
+        val scenario = launchFragmentInContainer<PlaylistSongsFragment>(bundle)
+        Espresso.onView(withId(R.id.SongRecyclerView)).check(matches(isDisplayed()))
     }
 }
