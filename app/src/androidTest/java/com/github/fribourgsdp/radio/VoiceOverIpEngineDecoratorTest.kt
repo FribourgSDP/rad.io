060310c94e6b0e37e441ea8da1611d01b7b96a9d
nathanduchesne <61148298+nathanduchesne@users.noreply.github.com>
2022-04-05 16:41:49 +0200

    Replaced mock implementation of RtcEngine with mockito

--- app/src/androidTest/java/com/github/fribourgsdp/radio/VoiceOverIpEngineDecoratorTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/VoiceOverIpEngineDecoratorTest.kt
@@ -7,17 +7,29 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import com.github.fribourgsdp.radio.mockimplementations.MockRtcEngine
+import io.agora.rtc.RtcEngine
 import junit.framework.Assert.assertTrue
 import org.hamcrest.Matchers
 import org.junit.Test
+import org.mockito.Mockito.*
+
 
 class VoiceOverIpEngineDecoratorTest {
+
+    fun makeMockRtcEngine() : RtcEngine {
+        val mockEngine = mock(RtcEngine::class.java)
+        `when`(mockEngine.joinChannel(any(), any(), any(), any())).thenReturn(0)
+        `when`(mockEngine.joinChannel(any(), any(), any(), any(), any())).thenReturn(0)
+        `when`(mockEngine.enableAudioVolumeIndication(any(), any(), any())).thenReturn(0)
+        `when`(mockEngine.setAudioProfile(any(), any())).thenReturn(0)
+        return mockEngine
+    }
+
     @Test
     fun initMockRtcEngine() {
         val context: Context = ApplicationProvider.getApplicationContext()
-        val mockEngine = MockRtcEngine()
-        val testEngineDecorator : VoiceIpEngineDecorator = VoiceIpEngineDecorator(context, mockEngine)
+
+        val testEngineDecorator : VoiceIpEngineDecorator = VoiceIpEngineDecorator(context, makeMockRtcEngine())
         val intent: Intent = Intent(context, VoiceOverIPActivity::class.java)
         ActivityScenario.launch<VoiceOverIPActivity>(intent).use { scenario ->
             Espresso.pressBack()
@@ -33,8 +45,7 @@ class VoiceOverIpEngineDecoratorTest {
     @Test
     fun allFunctionsWeNeedToCallReturnSuccess(){
         val context: Context = ApplicationProvider.getApplicationContext()
-        val mockEngine = MockRtcEngine()
-        val testEngineDecorator : VoiceIpEngineDecorator = VoiceIpEngineDecorator(context, mockEngine)
+        val testEngineDecorator : VoiceIpEngineDecorator = VoiceIpEngineDecorator(context, makeMockRtcEngine())
         assertTrue(testEngineDecorator.joinChannel(null, null, null, 3) == 0)
         assertTrue(testEngineDecorator.joinChannel(null, null, null, 3, null) == 0)
         assertTrue(testEngineDecorator.enableAudioVolumeIndication(5, 3, true) == 0)
