1a999748acad32d830ea9d5b837d5ba48cdac538
nathanduchesne <61148298+nathanduchesne@users.noreply.github.com>
2022-04-14 18:01:00 +0200

    Added test to check for mic permission callback

--- app/src/androidTest/java/com/github/fribourgsdp/radio/WorkingLobbyActivityTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/WorkingLobbyActivityTest.kt
@@ -1,25 +1,29 @@
 package com.github.fribourgsdp.radio
 
-import com.github.fribourgsdp.radio.mockimplementations.*
-
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
+import com.github.fribourgsdp.radio.mockimplementations.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.hamcrest.Matchers.not
-import org.junit.Before
 import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Lobby Activity Tests with working database
@@ -138,4 +142,43 @@ class WorkingLobbyActivityTest {
         }
     }
 
+    @Test
+    fun pressingOnEnablePermissionsRequestsThem() {
+        // Test values
+        val testUID = 42L
+        val testName = "Hello World!"
+        val testPlaylist = Playlist("Rap Playlist")
+        val testNbRounds = 20
+        val withHint = true
+        val private = true
+
+        val testIntent = Intent(ctx, WorkingLobbyActivity::class.java).apply {
+            putExtra(GAME_UID_KEY, testUID)
+            putExtra(GAME_NAME_KEY, testName)
+            putExtra(GAME_PLAYLIST_KEY, Json.encodeToString(testPlaylist))
+            putExtra(GAME_NB_ROUNDS_KEY, testNbRounds)
+            putExtra(GAME_HINT_KEY, withHint)
+            putExtra(GAME_PRIVACY_KEY, private)
+            putExtra(GAME_IS_HOST_KEY, false)
+        }
+        ActivityScenario.launch<LobbyActivity>(testIntent)
+        val micButton = Espresso.onView(withId(R.id.micPermissionsButton))
+        micButton.perform(ViewActions.click())
+        val instrumentation = getInstrumentation()
+        if (Build.VERSION.SDK_INT >= 23) {
+            val allowPermission = UiDevice.getInstance(instrumentation).findObject(
+                UiSelector().text(
+                    when {
+                        Build.VERSION.SDK_INT == 23 -> "Allow"
+                        Build.VERSION.SDK_INT <= 28 -> "ALLOW"
+                        Build.VERSION.SDK_INT == 29 -> "Allow only while using the app"
+                        else -> "While using the app"
+                    }
+                )
+            )
+            if (allowPermission.exists()) {
+                allowPermission.click()
+            }
+        }
+    }
 }
