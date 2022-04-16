e169876d696eb17ef79912a07ac60940d06254bb
bart <lucien.bart@epfl.ch>
2022-04-16 17:59:05 +0200

    Add skeleton to be able to mock the Firestore database

--- app/src/androidTest/java/com/github/fribourgsdp/radio/FirestoreDatabaseTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/FirestoreDatabaseTest.kt
@@ -3,20 +3,48 @@ package com.github.fribourgsdp.radio
 import android.content.ContentValues
 import android.util.Log
 import com.google.android.gms.tasks.Tasks
+import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
 import org.junit.Test
 import java.util.concurrent.TimeUnit
 import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.*
+
 class FirestoreDatabaseTest {
-    private val userAuthUID = "testUser"
-   
+    private val userAuthUID = "tnestUserUser"
+    private lateinit var mockFirestoreRef: FirestoreRef
+    private lateinit var mockSnapshot: DocumentSnapshot
+    private lateinit var mockDocumentReference : DocumentReference
+    private lateinit var db : FirestoreDatabase
+    @Before
+    fun setup(){
+        mockFirestoreRef = mock(FirestoreRef::class.java)
+        mockSnapshot = mock(DocumentSnapshot::class.java)
+        mockDocumentReference = mock(DocumentReference::class.java)
+        db = FirestoreDatabase(mockFirestoreRef)
+
+        //this handling is always the same, what changes is what the document snapshot returns
+        `when`(mockDocumentReference.get()).thenReturn(Tasks.forResult(mockSnapshot))
+        `when`(mockDocumentReference.set(anyMap<String,String>())).thenReturn( Tasks.whenAll(listOf(Tasks.forResult(3))))
+
+        `when`(mockFirestoreRef.getUserRef(userAuthUID)).thenReturn(mockDocumentReference)
+
+    }
     @Test
-    fun registeringUsingAndFetchingItWorks(){
-        val db = FirestoreDatabase()
-        val name = "TestingThings"
+    fun registeringUserAndFetchingItWorks(){
+        val name = "nathanael"
         val userTest = User(name)
+
+        `when`(mockSnapshot.exists()).thenReturn(true)
+        `when`(mockSnapshot.get("first")).thenReturn("nathanael")
+
         db.setUser(userAuthUID,userTest)
-        val user = Tasks.withTimeout(db.getUser(userAuthUID),10,TimeUnit.SECONDS)
-        assertEquals(name,Tasks.await(user).name)
+        val t1 = db.getUser(userAuthUID)
+
+        assertEquals(name,Tasks.await(t1).name)
     }
 
     @Test
