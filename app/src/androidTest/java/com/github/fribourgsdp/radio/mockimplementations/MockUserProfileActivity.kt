d578e8cecf665de9e8a1c141034026a814167671
Shanzhaiii <alexandre.riou@epfl.ch>
2022-04-17 18:36:26 +0200

    adding MockUserProfileActivity.kt

--- app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockUserProfileActivity.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockUserProfileActivity.kt
@@ -1,4 +1,21 @@
 package com.github.fribourgsdp.radio.mockimplementations
 
-class MockUserProfileActivity {
+import android.content.Context
+import android.os.Bundle
+import com.github.fribourgsdp.radio.*
+import org.mockito.Mockito
+
+class MockUserProfileActivity : UserProfileActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val context = Mockito.mock(Context::class.java)
+        MockFileSystem.wipeData()
+        User.setFSGetter(MockFileSystem.MockFSGetter)
+        val string = "test"
+        val user = User(string, 0)
+        val playlistTitle = "testTitle"
+        val playlist1 = Playlist(playlistTitle, Genre.ROCK)
+        user.addPlaylists(setOf(playlist1))
+        user.save(context)
+        super.onCreate(savedInstanceState)
+    }
 }
