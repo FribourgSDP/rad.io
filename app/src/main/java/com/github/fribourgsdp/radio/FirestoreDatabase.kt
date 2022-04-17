675829e0e2a8f1e890bd6aed0096e92036fadc98
Flamantrose321 <92850968+Flamantrose321@users.noreply.github.com>
2022-04-17 20:25:18 +0200

    remove major duplication (#160)

--- app/src/main/java/com/github/fribourgsdp/radio/FirestoreDatabase.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/FirestoreDatabase.kt
@@ -103,33 +103,18 @@ class FirestoreDatabase : Database {
     }
 
     override fun getLobbyId() : Task<Long> {
-        val keyID = "current_id"
-        val keyMax = "max_id"
-
-        val docRef = db.collection("lobby").document("id")
-
-        return db.runTransaction { transaction ->
-            val snapshot = transaction.get(docRef)
-
-            if (!snapshot.exists()) {
-                throw Exception("Document not found.")
-            }
-
-            val id = snapshot.getLong(keyID)!!
-            val nextId = (id + 1) % snapshot.getLong(keyMax)!!
-
-            transaction.update(docRef, keyID, nextId)
-
-            // Success
-            id
-        }
+        return getId("lobby","id")
     }
 
     override fun generateUserId() : Task<Long> {
+        return getId("metadata","UserInfo")
+    }
+
+    private fun getId(collectionPath : String, documentPath : String ) : Task<Long>{
         val keyID = "current_id"
         val keyMax = "max_id"
 
-        val docRef = db.collection("metadata").document("UserInfo")
+        val docRef = db.collection(collectionPath).document(documentPath)
 
         return db.runTransaction { transaction ->
             val snapshot = transaction.get(docRef)
@@ -147,6 +132,7 @@ class FirestoreDatabase : Database {
             id
         }
     }
+
 
     override fun openLobby(id: Long, settings : Game.Settings) : Task<Void> {
         val gameData = hashMapOf(
@@ -167,8 +153,7 @@ class FirestoreDatabase : Database {
     }
 
     override fun listenToLobbyUpdate(id: Long, listener: EventListener<DocumentSnapshot>) {
-        db.collection("lobby").document(id.toString())
-            .addSnapshotListener(listener)
+        listenUpdate("lobby", id, listener)
     }
 
     override fun getGameSettingsFromLobby(id: Long) :Task<Game.Settings> {
@@ -272,13 +257,11 @@ class FirestoreDatabase : Database {
     }
 
     override fun listenToGameUpdate(id: Long, listener: EventListener<DocumentSnapshot>) {
-        db.collection("games").document(id.toString())
-            .addSnapshotListener(listener)
+        listenUpdate("games", id, listener)
     }
 
     override fun listenToGameMetadataUpdate(id: Long, listener: EventListener<DocumentSnapshot>) {
-        db.collection("games_metadata").document(id.toString())
-            .addSnapshotListener(listener)
+        listenUpdate("games_metadata", id, listener)
     }
 
     override fun updateGame(id: Long, updatesMap: Map<String, Any>): Task<Void> {
@@ -325,5 +308,11 @@ class FirestoreDatabase : Database {
             null
         }
     }
+
+    private fun listenUpdate(collectionPath : String, id: Long, listener: EventListener<DocumentSnapshot>){
+        db.collection(collectionPath).document(id.toString())
+            .addSnapshotListener(listener)
+    }
+
 
 }
