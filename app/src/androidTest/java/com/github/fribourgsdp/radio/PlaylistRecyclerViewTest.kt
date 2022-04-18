17d166023d352b83b59207dd20266e7b1a84fe7c
Shanzhaiii <alexandre.riou@epfl.ch>
2022-04-18 02:17:15 +0200

    add final full Playlist exploration test

--- app/src/androidTest/java/com/github/fribourgsdp/radio/PlaylistRecyclerViewTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/PlaylistRecyclerViewTest.kt
@@ -6,13 +6,17 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.fribourgsdp.radio.mockimplementations.MockUserProfileActivity
+import com.github.fribourgsdp.radio.mockimplementations.playListName
+import com.github.fribourgsdp.radio.mockimplementations.songName
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.serialization.encodeToString
@@ -49,7 +53,6 @@ class PlaylistRecyclerViewTest {
         val intent = Intent(context, MockUserProfileActivity::class.java)
 
         ActivityScenario.launch<MockUserProfileActivity>(intent).use { scenario ->
-
             Espresso.onView(withId(R.id.playlist_recycler_view)).check(matches(isDisplayed()))
         }
     }
@@ -72,7 +75,34 @@ class PlaylistRecyclerViewTest {
         ActivityScenario.launch<MockUserProfileActivity>(intent).use { scenario ->
             Espresso.onView(withId(R.id.playlist_recycler_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
-            Espresso.onView(withId(R.id.PlaylistName)).check(matches(withText("testTitle")))
+            Espresso.onView(withId(R.id.PlaylistName)).check(matches(withText(playListName)))
+        }
+    }
+
+    @Test
+    fun recyclerViewTestFullDepth() {
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val task = Tasks.withTimeout(
+            firebaseAuth.signInWithEmailAndPassword(
+                "test@test.com",
+                "test123!!!"
+            ), 10, TimeUnit.SECONDS
+        )
+        Tasks.await(task)
+
+        val context: Context = ApplicationProvider.getApplicationContext()
+
+        val intent = Intent(context, MockUserProfileActivity::class.java)
+
+        ActivityScenario.launch<MockUserProfileActivity>(intent).use { scenario ->
+            Espresso.onView(withId(R.id.playlist_recycler_view))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+            Espresso.onView(withId(R.id.PlaylistName)).check(matches(withText(playListName)))
+            Espresso.onView(withId(R.id.SongRecyclerView)).check(matches(isDisplayed()))
+            Espresso.onView(withId(R.id.SongRecyclerView))
+                .perform(RecyclerViewActions.actionOnItemAtPosition<ViewHolder>(0, click()))
+            Espresso.onView(ViewMatchers.withId(R.id.SongName))
+                .check(ViewAssertions.matches(ViewMatchers.withText(songName)))
         }
     }
 }
