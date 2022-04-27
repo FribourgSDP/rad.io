34a3ab7933f4d57f26317a2e88e03ff36973eb33
Simon A. Marti <simon.marti@epfl.ch>
2022-04-27 15:21:59 +0200

    Add simple test for lyricspopup

---
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/LyricsPopupTest.kt
@@ -0,0 +1,27 @@
+package com.github.fribourgsdp.radio
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragment
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.isDialog
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.Matchers.not
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class LyricsPopupTest {
+
+    @Test
+    fun testCorrectTextIsDisplayed(){
+        with(launchFragment(
+            themeResId = R.style.Theme_Radio,
+            instantiate = {LyricsPopup("Lorem Ipsum")})) {
+            onView(withId(R.id.lyricsPopupTextView)).check(matches(withText("Lorem Ipsum")))
+        }
+    }
+}
