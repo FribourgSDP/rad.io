b852efe02cb7debc5fd0e439fac513612d54fa3c
bart <lucien.bart@epfl.ch>
2022-04-28 19:17:34 +0200

    Change name to correspond to new name in xml

--- app/src/main/java/com/github/fribourgsdp/radio/SavePlaylistOnlinePickerDialog.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/SavePlaylistOnlinePickerDialog.kt
@@ -9,18 +9,15 @@ import androidx.fragment.app.DialogFragment
 
 class SavePlaylistOnlinePickerDialog( private val listener: OnPickListener): DialogFragment() {
 
-
-    private val buttons = ArrayList<Button>()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val rootView: View = inflater.inflate(R.layout.fragment_playlist_save_online_picker, container, false)
 
         // Add the buttons of the view to the list
-        rootView.findViewById<Button>(R.id.saveOnlinePlaylist).setOnClickListener{
+        rootView.findViewById<Button>(R.id.saveOnlinePlaylistYes).setOnClickListener{
             listener.onPick(true)
             dismiss()
         }
-       rootView.findViewById<Button>(R.id.saveOfflinePlaylist).setOnClickListener{
+       rootView.findViewById<Button>(R.id.saveOnlinePlaylistNo).setOnClickListener{
             listener.onPick(false)
             dismiss()
         }
