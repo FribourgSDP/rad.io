93f6ad4e1493016b9d9efcaa0d3217b09f7d3f7f
mmauron <maxence.mauron@epfl.ch>
2022-05-02 14:59:28 +0200

    add test for scan qr code fragment

--- app/src/androidTest/java/com/github/fribourgsdp/radio/WorkingJoinGameActivityTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/WorkingJoinGameActivityTest.kt
@@ -1,15 +1,20 @@
 package com.github.fribourgsdp.radio
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.matcher.RootMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.GrantPermissionRule
 import com.github.fribourgsdp.radio.mockimplementations.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -29,8 +34,12 @@ import org.mockito.Mockito
  */
 @RunWith(AndroidJUnit4::class)
 class WorkingJoinGameActivityTest {
+
     @get:Rule
-    var gameSettingsActivityRule = ActivityScenarioRule(WorkingJoinGameActivity::class.java)
+    var mRuntimePermissionRule = GrantPermissionRule.grant(Manifest.permission.CAMERA)
+    @get:Rule
+    var mRuntimePermissionRule2 = GrantPermissionRule.grant(Manifest.permission.VIBRATE)
+
 
     @Before
     fun initIntent() {
@@ -47,42 +56,66 @@ class WorkingJoinGameActivityTest {
         // Test values => this one cannot exist, so it won't look in the database
         val testUID = 1001L
 
-        val joinButton = Espresso.onView(ViewMatchers.withId(R.id.joinGameButton))
+        val context: Context = ApplicationProvider.getApplicationContext()
+        val intent = Intent(context, WorkingJoinGameActivity::class.java)
+        ActivityScenario.launch<WorkingJoinGameActivity>(intent).use { scenario ->
 
-        joinButton.check(
-            ViewAssertions.matches(
-                ViewMatchers.isNotEnabled()
+            val joinButton = Espresso.onView(ViewMatchers.withId(R.id.joinGameButton))
+
+            joinButton.check(
+                ViewAssertions.matches(
+                    ViewMatchers.isNotEnabled()
+                )
             )
-        )
 
-        Espresso.onView(ViewMatchers.withId(R.id.gameToJoinID))
-            .perform(ViewActions.typeText(testUID.toString()))
+            Espresso.onView(ViewMatchers.withId(R.id.gameToJoinID))
+                .perform(ViewActions.typeText(testUID.toString()))
 
-        Espresso.closeSoftKeyboard()
+            Espresso.closeSoftKeyboard()
 
-        joinButton.check(
-            ViewAssertions.matches(
-                ViewMatchers.isEnabled()
+            joinButton.check(
+                ViewAssertions.matches(
+                    ViewMatchers.isEnabled()
+                )
             )
-        )
 
-        joinButton.perform(ViewActions.click())
+            joinButton.perform(ViewActions.click())
 
-        Intents.intended(
-            Matchers.allOf(
-                IntentMatchers.toPackage("com.github.fribourgsdp.radio"),
-                IntentMatchers.hasComponent(LobbyActivity::class.java.name),
-                IntentMatchers.hasExtra(GAME_HOST_NAME_KEY, LocalDatabase.EXPECTED_SETTINGS.hostName),
-                IntentMatchers.hasExtra(GAME_NAME_KEY, LocalDatabase.EXPECTED_SETTINGS.name),
-                IntentMatchers.hasExtra(GAME_PLAYLIST_NAME_KEY,LocalDatabase.EXPECTED_SETTINGS.playlistName),
-                IntentMatchers.hasExtra(GAME_NB_ROUNDS_KEY, LocalDatabase.EXPECTED_SETTINGS.nbRounds),
-                IntentMatchers.hasExtra(GAME_HINT_KEY, LocalDatabase.EXPECTED_SETTINGS.withHint),
-                IntentMatchers.hasExtra(GAME_PRIVACY_KEY, LocalDatabase.EXPECTED_SETTINGS.isPrivate),
-                IntentMatchers.hasExtra(GAME_IS_HOST_KEY, false),
-                IntentMatchers.hasExtra(GAME_UID_KEY, testUID)
-
+            Intents.intended(
+                Matchers.allOf(
+                    IntentMatchers.toPackage("com.github.fribourgsdp.radio"),
+                    IntentMatchers.hasComponent(LobbyActivity::class.java.name),
+                    IntentMatchers.hasExtra(GAME_HOST_NAME_KEY, LocalDatabase.EXPECTED_SETTINGS.hostName),
+                    IntentMatchers.hasExtra(GAME_NAME_KEY, LocalDatabase.EXPECTED_SETTINGS.name),
+                    IntentMatchers.hasExtra(GAME_PLAYLIST_NAME_KEY,LocalDatabase.EXPECTED_SETTINGS.playlistName),
+                    IntentMatchers.hasExtra(GAME_NB_ROUNDS_KEY, LocalDatabase.EXPECTED_SETTINGS.nbRounds),
+                    IntentMatchers.hasExtra(GAME_HINT_KEY, LocalDatabase.EXPECTED_SETTINGS.withHint),
+                    IntentMatchers.hasExtra(GAME_PRIVACY_KEY, LocalDatabase.EXPECTED_SETTINGS.isPrivate),
+                    IntentMatchers.hasExtra(GAME_IS_HOST_KEY, false),
+                    IntentMatchers.hasExtra(GAME_UID_KEY, testUID)
+                )
             )
-        )
+        }
+    }
+
+
+    @Test
+    fun cancelScanQRCodeWork(){
+
+        val context: Context = ApplicationProvider.getApplicationContext()
+        val intent = Intent(context, WorkingJoinGameActivity::class.java)
+        ActivityScenario.launch<WorkingJoinGameActivity>(intent).use { scenario ->
+            val displayQRCodeButton = Espresso.onView(ViewMatchers.withId(R.id.joinWithQRCode))
+            displayQRCodeButton.perform(ViewActions.click())
+
+            Espresso.onView(ViewMatchers.withId(R.id.cancel_button))
+                .inRoot(RootMatchers.isDialog())
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+            Espresso.onView(ViewMatchers.withId(R.id.cancel_button))
+                .inRoot(RootMatchers.isDialog())
+                .perform(ViewActions.click())
+        }
     }
 
 }
