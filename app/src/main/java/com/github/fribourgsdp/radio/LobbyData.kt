13c47fe69526dc50146e948901b0c98da5b52dd8
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-05-02 16:18:51 +0200

    Create lobby data keys enum

--- app/src/main/java/com/github/fribourgsdp/radio/LobbyData.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/LobbyData.kt
@@ -1,3 +1,9 @@
 package com.github.fribourgsdp.radio
 
 data class LobbyData(val id: Long, val name: String, val hostName: String)
+
+enum class LobbyDataKeys(stringId: Int) {
+    ID(R.string.id),
+    NAME(R.string.name),
+    HOSTNAME(R.string.hostname)
+}
