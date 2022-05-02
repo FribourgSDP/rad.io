ac7ca7c2e65d3c6b65170dc8caeab028e2910bbf
mmauron <maxence.mauron@epfl.ch>
2022-05-02 14:07:17 +0200

    remove task from SettingsTest.kt

--- app/src/androidTest/java/com/github/fribourgsdp/radio/SettingsBuilderTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/SettingsBuilderTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 class SettingsBuilderTest {
     @Test
     fun changeLanguageWork(){
-        val settings = Settings.createDefaultSettings().getResult()
+        val settings = Settings.createDefaultSettings()
         val settingsBuilder = SettingsBuilder(settings)
         val settings2 = settingsBuilder.language(Language.FRENCH).build()
         Assert.assertEquals(Language.FRENCH, settings2.getLanguage())
