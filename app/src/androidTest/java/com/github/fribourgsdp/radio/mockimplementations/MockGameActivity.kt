3cbaee046429f1f12f19c2f513319ba7c253cf80
mmauron <maxence.mauron@epfl.ch>
2022-05-02 17:25:42 +0200

    add test for scan qr code fragment

--- app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockGameActivity.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockGameActivity.kt
@@ -1,6 +1,9 @@
 package com.github.fribourgsdp.radio.mockimplementations
 
 import com.github.fribourgsdp.radio.*
+import io.agora.rtc.IRtcEngineEventHandler
+import io.agora.rtc.RtcEngine
+import org.mockito.Mockito
 
 
 class MockGameActivity : GameActivity() {
@@ -8,4 +11,33 @@ class MockGameActivity : GameActivity() {
         voiceChannel = VoiceIpEngineDecorator(this, makeMockIRtcEngineEventHandler(), makeMockRtcEngine())
         super.initVoiceChat(gameUid)
     }
+}
+
+
+fun makeMockRtcEngine() : RtcEngine {
+    val mockEngine = Mockito.mock(RtcEngine::class.java)
+    Mockito.`when`(
+        mockEngine.joinChannel(
+            Mockito.any(),
+            Mockito.any(),
+            Mockito.any(),
+            Mockito.anyInt()
+        )
+    ).thenReturn(0)
+    Mockito.`when`(
+        mockEngine.enableAudioVolumeIndication(
+            Mockito.anyInt(),
+            Mockito.anyInt(),
+            Mockito.anyBoolean()
+        )
+    ).thenReturn(0)
+    Mockito.`when`(mockEngine.setAudioProfile(Mockito.anyInt(), Mockito.anyInt())).thenReturn(0)
+    Mockito.`when`(mockEngine.leaveChannel()).thenReturn(0)
+    Mockito.`when`(mockEngine.muteLocalAudioStream(Mockito.anyBoolean())).thenReturn(0)
+    return mockEngine
+}
+
+fun makeMockIRtcEngineEventHandler() : IRtcEngineEventHandler {
+    val iRtcEngineEventHandler = Mockito.mock(IRtcEngineEventHandler::class.java)
+    return iRtcEngineEventHandler
 }
