da09ea3af70f553678f52101851d14e343631607
bart <lucien.bart@epfl.ch>
2022-05-04 09:37:17 +0200

    Create dialog to choose what to do when connecting with google

---
+++ app/src/main/java/com/github/fribourgsdp/radio/MergeDismissImportPlaylistDialog.kt
@@ -0,0 +1,39 @@
+package com.github.fribourgsdp.radio
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+
+class MergeDismissImportPlaylistDialog (private val listener: OnPickListener) : DialogFragment()
+{
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val rootView: View = inflater.inflate(R.layout.fragment_playlist_merge_import_dismiss_picker, container, false)
+
+        // Add the buttons of the view to the list
+        rootView.findViewById<Button>(R.id.mergePlaylistButton).setOnClickListener{
+            listener.onPick(Choice.MERGE)
+            dismiss()
+        }
+        rootView.findViewById<Button>(R.id.importPlaylistButton).setOnClickListener{
+            listener.onPick(Choice.IMPORT)
+            dismiss()
+        }
+        rootView.findViewById<Button>(R.id.dismissOnlineButton).setOnClickListener{
+            listener.onPick(Choice.DISMISS_ONLINE)
+            dismiss()
+        }
+        return rootView
+    }
+
+    interface OnPickListener{
+        fun onPick(choice: Choice)
+    }
+
+    enum class Choice {
+        MERGE, IMPORT, DISMISS_ONLINE
+    }
+}
