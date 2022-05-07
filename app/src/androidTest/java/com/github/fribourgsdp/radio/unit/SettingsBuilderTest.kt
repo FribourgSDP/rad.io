79b6e10489bd3a7a1d1d38676552bb41e7a09012
Shanzhaiii <alexandre.riou@epfl.ch>
2022-05-07 19:22:30 +0200

    refactor SettingsBuilderTest.kt into /unit

--- app/src/androidTest/java/com/github/fribourgsdp/radio/unit/SettingsBuilderTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/unit/SettingsBuilderTest.kt
@@ -1,4 +1,4 @@
-package com.github.fribourgsdp.radio
+package com.github.fribourgsdp.radio.unit
 
 import com.github.fribourgsdp.radio.config.Settings
 import com.github.fribourgsdp.radio.config.SettingsBuilder
