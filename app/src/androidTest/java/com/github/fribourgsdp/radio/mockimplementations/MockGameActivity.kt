520d1b020586166febff0f64dcaef37d7554731d
shanzhaiii <56897608+shanzhaiii@users.noreply.github.com>
2022-05-07 21:12:23 +0200

    refactor unit tests into /unit (#231)
    * refactor unit tests into /unit
    
    * refactor activity tests into /activities
    
    * refactor SettingsBuilderTest.kt into /unit
    
    * refactor HostGameHandlerTest.kt into /unit
    
    * refactor tests of external tools into /external
    
    * refactor PlayerGameHandlerTest.kt into /unit
    
    * refactor StringComparisonsTest.kt into /unit
    
    * refactor fragment, dialog, recyclerview tests into /ui
    
    * add mock FileSystem for IRtcEngine tests
    
    * add mock FileSystem for IRtcEngine tests

--- app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockGameActivity.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/mockimplementations/MockGameActivity.kt
@@ -1,10 +1,29 @@
 package com.github.fribourgsdp.radio.mockimplementations
 
-import com.github.fribourgsdp.radio.*
+import android.content.Context
+import android.os.Bundle
+import com.github.fribourgsdp.radio.data.Genre
+import com.github.fribourgsdp.radio.data.Playlist
+import com.github.fribourgsdp.radio.data.Song
+import com.github.fribourgsdp.radio.data.User
+import com.github.fribourgsdp.radio.external.makeMockIRtcEngineEventHandler
+import com.github.fribourgsdp.radio.external.makeMockRtcEngine
 import com.github.fribourgsdp.radio.game.GameActivity
 import com.github.fribourgsdp.radio.voip.VoiceIpEngineDecorator
+import org.mockito.Mockito
 
 class MockGameActivity : GameActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        User.setFSGetter(MockFileSystem.MockFSGetter)
+        val user = User(userName, 0)
+        val playlist1 = Playlist(playListName, Genre.ROCK)
+        val song = Song(songName, "artist")
+        playlist1.addSong(song)
+        user.addPlaylists(setOf(playlist1))
+        user.save(Mockito.mock(Context::class.java))
+        super.onCreate(savedInstanceState)
+    }
+
     override fun initVoiceChat(gameUid: Long) {
         voiceChannel = VoiceIpEngineDecorator(this, makeMockIRtcEngineEventHandler(), makeMockRtcEngine())
         super.initVoiceChat(gameUid)
---
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/unit/StringComparisonsTest.kt
@@ -0,0 +1,58 @@
+package com.github.fribourgsdp.radio.unit
+
+import org.junit.Test
+import org.junit.Assert.*
+import com.github.fribourgsdp.radio.util.StringComparisons
+
+class StringComparisonsTest {
+    @Test
+    fun equalStringsTest(){
+        assertEquals(0, StringComparisons.compare("rouge", "rouge"))
+    }
+    @Test
+    fun spacefulStringsTest(){
+        assertEquals(0, StringComparisons.compare("  wish you    were  here    ", "wish you were here"))
+    }
+    @Test
+    fun differentCasesStringsTest(){
+        assertEquals(0, StringComparisons.compare("  Wish you    WERE  herE    ", "wish you were here"))
+    }
+    @Test
+    fun veryDecoratedStringsTest(){
+        assertEquals(0,  StringComparisons.compare("  Wish, you  ~  WERE-herE    ", "Wish you were here..."))
+    }
+    @Test
+    fun accentedStrings(){
+        assertEquals(0, StringComparisons.compare("àäầéèêẽ-íïőucç", "aaaeeee_iiouçĉ"))
+    }
+    @Test
+    fun oneDifferenceStrings1(){
+        assertEquals(1, StringComparisons.compare("rovge", "rouge"))
+        assertEquals(1, StringComparisons.compare("rougf ", "rouge"))
+        assertEquals(1, StringComparisons.compare("rovge", "rouge"))
+        assertEquals(1, StringComparisons.compare("  Wish, yoO  ~  WERE-herE    ", "Wish you were here..."))
+        assertEquals(1, StringComparisons.compare("àûầéèêẽ-íïöucç", "aaaeeee_iiouçĉ"))
+        }
+    @Test
+    fun oneDifferenceStrings2(){
+        assertEquals(1, StringComparisons.compare("roge", "rouge"))
+        assertEquals(1, StringComparisons.compare("rouuge", "rouge"))
+        assertEquals(1, StringComparisons.compare("master of pupppets", "master of puppets"))
+        assertEquals(1, StringComparisons.compare("master of pupets", "master of puppets"))
+        assertEquals(1, StringComparisons.compare("master of puppet", "master of puppets"))
+    }
+    @Test
+    fun twoDifferencesStrings2(){
+        assertEquals(2, StringComparisons.compare("rge", "rouge"))
+        assertEquals(2, StringComparisons.compare("rouuuge", "rouge"))
+        assertEquals(2, StringComparisons.compare("mastwr og puppets", "master of puppets"))
+        assertEquals(2, StringComparisons.compare("master of pupet", "master of puppets"))
+        assertEquals(2, StringComparisons.compare("master of puppppets", "master of puppets"))
+    }
+    @Test
+    fun differentStrings(){
+        assertNotEquals(1, StringComparisons.compare("rge", "rouge"))
+        assertNotEquals(0, StringComparisons.compare("rougf", "rouge"))
+    }
+
+}
