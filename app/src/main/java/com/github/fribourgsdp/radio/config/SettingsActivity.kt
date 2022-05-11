35650b76d2f7252cd6e0a065513f7399d9a50e4d
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-05-11 12:32:26 +0200

    Change ordinal only when not default

--- app/src/main/java/com/github/fribourgsdp/radio/config/SettingsActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/config/SettingsActivity.kt
@@ -60,7 +60,9 @@ open class SettingsActivity : MyAppCompatActivity() {
         val adapter: ArrayAdapter<Language> = ArrayAdapter<Language>(this,android.R.layout.simple_spinner_item,languageList)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerLanguage.setAdapter(adapter);
-        spinnerLanguage.setSelection(actualLanguage.ordinal, false);
+        if (actualLanguage.ordinal != 0) {
+            spinnerLanguage.setSelection(actualLanguage.ordinal, false)
+        }
         spinnerLanguage.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?,view: View?,position: Int,id: Long) {
                 if (parent != null && view != null) {
