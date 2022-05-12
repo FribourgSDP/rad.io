0af6116383cb80d53f035046ba865f78f1c6aad1
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-05-12 14:09:34 +0200

    Reformat old tests

--- app/src/androidTest/java/com/github/fribourgsdp/radio/activities/EndGameActivityTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/activities/EndGameActivityTest.kt
@@ -2,11 +2,10 @@ package com.github.fribourgsdp.radio.activities
 
 import android.content.Context
 import android.content.Intent
-import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
@@ -20,7 +19,7 @@ import com.github.fribourgsdp.radio.game.EndGameActivity
 import com.github.fribourgsdp.radio.game.GAME_CRASH_KEY
 import com.github.fribourgsdp.radio.game.SCORES_KEY
 import com.github.fribourgsdp.radio.utils.CustomMatchers.Companion.atPosition
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.allOf
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -47,10 +46,10 @@ class EndGameActivityTest{
     @Test
     fun onBackPressedGoesToMainMenu() {
         val intent = Intent(ctx, EndGameActivity::class.java)
-        ActivityScenario.launch<EndGameActivity>(intent).use{ _ ->
-            Espresso.pressBack()
+        launch<EndGameActivity>(intent).use{ _ ->
+            pressBack()
             Intents.intended(
-                Matchers.allOf(
+                allOf(
                     IntentMatchers.hasComponent(MainActivity::class.java.name)
                 )
             )
@@ -68,12 +67,12 @@ class EndGameActivityTest{
         val testIntent = Intent(ctx, EndGameActivity::class.java).apply {
             putExtra(SCORES_KEY, scores)
         }
-        ActivityScenario.launch<EndGameActivity>(testIntent).use {
+        launch<EndGameActivity>(testIntent).use {
             // Check that the scores are displayed with the correct data and in the correct order
-            Espresso.onView(ViewMatchers.withId(R.id.endScoresRecyclerView))
+            onView(withId(R.id.endScoresRecyclerView))
                 .check(
-                    ViewAssertions.matches(
-                        Matchers.allOf(
+                    matches(
+                        allOf(
                             atPosition(0, R.id.nameScoreTextView, ViewMatchers.withText("singer2")),
                             atPosition(0, R.id.scoreTextView, ViewMatchers.withText("100")),
                             atPosition(1, R.id.nameScoreTextView, ViewMatchers.withText("singer0")),
