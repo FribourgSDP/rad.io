bf32513ed76e9c65a614f40867cadd2354f2fff7
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-05-24 16:55:48 +0200

    Add test for custom splash screen

--- app/src/androidTest/java/com/github/fribourgsdp/radio/activities/SplashScreenTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/activities/SplashScreenTest.kt
@@ -1,17 +1,52 @@
-package com.github.fribourgsdp.radio
+package com.github.fribourgsdp.radio.activities
 
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.fribourgsdp.radio.MainActivity
+import com.github.fribourgsdp.radio.SplashScreen
+import com.github.fribourgsdp.radio.game.prep.GameSettingsActivity
+import org.hamcrest.Matchers
 import org.junit.After
 import org.junit.Before
 
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
+/**
+ * Splash screen Tests
+ *
+ */
+@RunWith(AndroidJUnit4::class)
 class SplashScreenTest {
 
+    @get:Rule
+    var splashRule = ActivityScenarioRule(SplashScreen::class.java)
+
     @Before
-    fun setUp() {
+    fun initIntent() {
+        Intents.init()
     }
 
     @After
-    fun tearDown() {
+    fun releaseIntent() {
+        Intents.release()
     }
+
+    @Test
+    fun splashScreenSendsToMain() {
+        // wait for the screen to finish
+        Thread.sleep(SplashScreen.LOAD_TIME_IN_MILLIS + 1)
+
+        Intents.intended(
+            Matchers.allOf(
+                IntentMatchers.hasComponent(MainActivity::class.java.name),
+                IntentMatchers.toPackage("com.github.fribourgsdp.radio")
+            )
+        )
+    }
+
 }
