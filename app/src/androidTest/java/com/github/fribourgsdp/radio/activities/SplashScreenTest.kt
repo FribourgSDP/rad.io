cd533449d76d7cbbe8b40108091fe47e64ecd8a3
Victor Schneuwly <victor.schneuwly@epfl.ch>
2022-05-24 16:55:33 +0200

    Add custom splash screen

---
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/activities/SplashScreenTest.kt
@@ -0,0 +1,17 @@
+package com.github.fribourgsdp.radio
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class SplashScreenTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
