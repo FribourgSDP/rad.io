25966e297df30641a76a880e582becfad5afcd37
bart <lucien.bart@epfl.ch>
2022-05-26 19:01:56 +0200

    Extract the connectivity interface

--- app/src/main/java/com/github/fribourgsdp/radio/config/MyAppCompatActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/config/MyAppCompatActivity.kt
@@ -8,30 +8,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.fribourgsdp.radio.config.language.LanguageManager
 
-open class MyAppCompatActivity : AppCompatActivity() {
+open class MyAppCompatActivity : AppCompatActivity(),ConnectivityHolder {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val languageManager = LanguageManager(this)
         languageManager.getLang()?.let { languageManager.updateResource(it) }
     }
 
-    fun hasConnectivity(context: Context): Boolean {
 
-        // register activity with the connectivity manager service
-        val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val network = connectivityManager.activeNetwork ?: return false
-
-        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-        return when {
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-
-            else -> false
-        }
-
-    }
 }
