6000dc8ceb0486d61d4d5b3f1731b22e4837cef2
bart <lucien.bart@epfl.ch>
2022-05-26 21:51:05 +0200

    Make it impossible to click on online playlist while offline

--- app/src/main/java/com/github/fribourgsdp/radio/data/view/UserPlaylistsFragment.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/data/view/UserPlaylistsFragment.kt
@@ -3,6 +3,7 @@ package com.github.fribourgsdp.radio.data.view
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultCaller.*
 import androidx.activity.result.ActivityResultLauncher
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.github.fribourgsdp.radio.util.MyFragment
 import com.github.fribourgsdp.radio.util.OnClickListener
 import com.github.fribourgsdp.radio.R
+import com.github.fribourgsdp.radio.config.ConnectivityChecker
 import com.github.fribourgsdp.radio.data.Playlist
 import com.github.fribourgsdp.radio.data.User
 import kotlinx.serialization.decodeFromString
@@ -20,7 +22,7 @@ import kotlinx.serialization.json.Json
 const val PLAYLIST_DATA = "com.github.fribourgsdp.radio.PLAYLIST_INNER_DATA"
 
 class UserPlaylistsFragment : MyFragment(R.layout.fragment_user_playlists_display),
-    OnClickListener {
+    OnClickListener,ConnectivityChecker {
     private lateinit var user: User
     private lateinit var userPlaylists: List<Playlist>
     private lateinit var songDisplay: RecyclerView
@@ -44,9 +46,15 @@ class UserPlaylistsFragment : MyFragment(R.layout.fragment_user_playlists_displa
 
     override fun onItemClick(position: Int) {
         activity?.run {
-            val intent = Intent(this, PlaylistsFragmentHolderActivity::class.java)
-                .putExtra(PLAYLIST_DATA, userPlaylists[position].name)
-            startForResult.launch(intent)
+            if(hasConnectivity(requireContext()) && userPlaylists[position].savedOnline){
+                val intent = Intent(this, PlaylistsFragmentHolderActivity::class.java)
+                    .putExtra(PLAYLIST_DATA, userPlaylists[position].name)
+                startForResult.launch(intent)
+            }else{
+                Toast.makeText(this,"Cannot access online data without connection", Toast.LENGTH_SHORT).show()
+
+            }
+
         }
     }
 
