fe7f8b0fda8e8f72c5e5e9872d5c275bd775fae0
bart <lucien.bart@epfl.ch>
2022-05-26 14:06:16 +0200

    Add function to check for connectivity

--- app/src/main/java/com/github/fribourgsdp/radio/config/MyAppCompatActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/config/MyAppCompatActivity.kt
@@ -1,5 +1,9 @@
 package com.github.fribourgsdp.radio.config
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.fribourgsdp.radio.config.language.LanguageManager
@@ -7,7 +11,27 @@ import com.github.fribourgsdp.radio.config.language.LanguageManager
 open class MyAppCompatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val languageManager  = LanguageManager(this)
+        val languageManager = LanguageManager(this)
         languageManager.getLang()?.let { languageManager.updateResource(it) }
+    }
+
+    fun hasConnectivity(context: Context): Boolean {
+
+        // register activity with the connectivity manager service
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val network = connectivityManager.activeNetwork ?: return false
+
+        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+        return when {
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+            else -> false
+        }
+
     }
 }
