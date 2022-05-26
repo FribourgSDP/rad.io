85f7431c2b892afcf702b7e56bf33c5490d5bd3c
Simon Marti <40426105+sim0mo@users.noreply.github.com>
2022-05-26 12:58:16 +0200

    Update GameActivity.kt

--- app/src/main/java/com/github/fribourgsdp/radio/game/GameActivity.kt
+++ app/src/main/java/com/github/fribourgsdp/radio/game/GameActivity.kt
@@ -38,7 +38,6 @@ import kotlin.math.ceil
 const val SCORES_KEY = "com.github.fribourgsdp.radio.SCORES"
 const val GAME_CRASH_KEY = "com.github.fribourgsdp.radio.GAME_CRASH"
 const val HINT_FRACTION = 1.0/3
-const val GAME_OVER_SCREEN_DELAY : Long = 1000
 
 open class GameActivity : AppCompatActivity(), GameView, Timer.Listener {
     private lateinit var user: User
@@ -227,9 +226,6 @@ open class GameActivity : AppCompatActivity(), GameView, Timer.Listener {
 
             putExtra(GAME_CRASH_KEY, hasCrashed)
         }
-
-        //delays last screen
-        Thread.sleep(GAME_OVER_SCREEN_DELAY)
         startActivity(intent)
     }
 
