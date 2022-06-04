a97b6138f3fc392a76631311bb582a20231621a0
Simon A. Marti <simon.marti@epfl.ch>
2022-06-04 11:14:30 +0200

    Keep all strings in StringComparisonsTest.kt

--- app/src/androidTest/java/com/github/fribourgsdp/radio/unit/StringComparisonsTest.kt
+++ app/src/androidTest/java/com/github/fribourgsdp/radio/unit/StringComparisonsTest.kt
@@ -1,6 +1,5 @@
 package com.github.fribourgsdp.radio.unit
 
-import com.github.fribourgsdp.radio.activities.testSong1
 import org.junit.Test
 import org.junit.Assert.*
 import com.github.fribourgsdp.radio.util.StringComparisons
@@ -8,7 +7,7 @@ import com.github.fribourgsdp.radio.util.StringComparisons
 class StringComparisonsTest {
     @Test
     fun equalStringsTest(){
-        assertEquals(0, StringComparisons.compare(testSong1, testSong1))
+        assertEquals(0, StringComparisons.compare("rouge", "rouge"))
     }
     @Test
     fun spacefulStringsTest(){
@@ -28,32 +27,32 @@ class StringComparisonsTest {
     }
     @Test
     fun oneDifferenceStrings1(){
-        assertEquals(1, StringComparisons.compare("rovge", testSong1))
-        assertEquals(1, StringComparisons.compare("rougf ", testSong1))
-        assertEquals(1, StringComparisons.compare("rovge", testSong1))
+        assertEquals(1, StringComparisons.compare("rovge", "rouge"))
+        assertEquals(1, StringComparisons.compare("rougf ", "rouge"))
+        assertEquals(1, StringComparisons.compare("rovge", "rouge"))
         assertEquals(1, StringComparisons.compare("  Wish, yoO  ~  WERE-herE    ", "Wish you were here..."))
         assertEquals(1, StringComparisons.compare("àûầéèêẽ-íïöucç", "aaaeeee_iiouçĉ"))
         }
     @Test
     fun oneDifferenceStrings2(){
-        assertEquals(1, StringComparisons.compare("roge", testSong1))
-        assertEquals(1, StringComparisons.compare("rouuge", testSong1))
+        assertEquals(1, StringComparisons.compare("roge", "rouge"))
+        assertEquals(1, StringComparisons.compare("rouuge", "rouge"))
         assertEquals(1, StringComparisons.compare("master of pupppets", "master of puppets"))
         assertEquals(1, StringComparisons.compare("master of pupets", "master of puppets"))
         assertEquals(1, StringComparisons.compare("master of puppet", "master of puppets"))
     }
     @Test
     fun twoDifferencesStrings2(){
-        assertEquals(2, StringComparisons.compare("rge", testSong1))
-        assertEquals(2, StringComparisons.compare("rouuuge", testSong1))
+        assertEquals(2, StringComparisons.compare("rge", "rouge"))
+        assertEquals(2, StringComparisons.compare("rouuuge", "rouge"))
         assertEquals(2, StringComparisons.compare("mastwr og puppets", "master of puppets"))
         assertEquals(2, StringComparisons.compare("master of pupet", "master of puppets"))
         assertEquals(2, StringComparisons.compare("master of puppppets", "master of puppets"))
     }
     @Test
     fun differentStrings(){
-        assertNotEquals(1, StringComparisons.compare("rge", testSong1))
-        assertNotEquals(0, StringComparisons.compare("rougf", testSong1))
+        assertNotEquals(1, StringComparisons.compare("rge", "rouge"))
+        assertNotEquals(0, StringComparisons.compare("rougf", "rouge"))
     }
 
 }
